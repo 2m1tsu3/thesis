092a6ef12590dc053b85b998c9f07cbf9089e15b
uk@example.com <uk@example.com>
2015-06-23 17:59:45 +0900

    Revert "create port.py"
    This reverts commit 76f2f156adafc5c5759c3898a2efe7817dbb89e2.

--- subnet.py
+++ subnet.py
@@ -17,22 +17,20 @@ subnet = {}
 
 
 for subnet_data in input_data['networks']:
-    subnet_name = subnet_data['name']+"_subnet"
-    print(subnet_name)
-    subnet[subnet_name] = {'type': 'OS::Neutron::Subnet','depends_on':'','properties':{'name':'','network_id':{'get_resource':''},'cidr':'','gateway_ip':'','dns_nameservers':[],'allocation_pools':[{'start':'','end':''}]}}
-    subnet[subnet_name]['depends_on'] = subnet_data['name']
-    subnet[subnet_name]['properties']['name'] = subnet_name
-    subnet[subnet_name]['properties']['network_id']['get_resource'] = subnet_data['name']
-    subnet[subnet_name]['properties']['cidr'] = subnet_data['cidr']
+    subnet[subnet_data['name']+"_subnet"] = {'type': 'OS::Neutron::Subnet','depends_on':'','properties':{'name':'','network_id':{'get_resource':''},'cidr':'','gateway_ip':'','dns_nameservers':[],'allocation_pools':[{'start':'','end':''}]}}
+    subnet[subnet_data['name']+"_subnet"]['depends_on'] = subnet_data['name']
+    subnet[subnet_data['name']+"_subnet"]['properties']['name'] = subnet_data['name']+"_subnet"
+    subnet[subnet_data['name']+"_subnet"]['properties']['network_id']['get_resource'] = subnet_data['name']
+    subnet[subnet_data['name']+"_subnet"]['properties']['cidr'] = subnet_data['cidr']
     address = ipaddress.ip_network(subnet_data['cidr'])
-    subnet[subnet_name]['properties']['gateway_ip'] = str(address[1])
+    subnet[subnet_data['name']+"_subnet"]['properties']['gateway_ip'] = str(address[1])
     if not subnet_data['dns']:
-        subnet[subnet_name]['properties']['dns_nameservers'].append("8.8.8.8")
+        subnet[subnet_data['name']+"_subnet"]['properties']['dns_nameservers'].append("8.8.8.8")
     else:
         for dns_data in subnet_data['dns']:
-            subnet[subnet_name]['properties']['dns_nameservers'].append(dns_data)
-    subnet[subnet_name]['properties']['allocation_pools'][0]['start'] = str(address[2])
-    subnet[subnet_name]['properties']['allocation_pools'][0]['end'] = str(address[-2])
+            subnet[subnet_data['name']+"_subnet"]['properties']['dns_nameservers'].append(dns_data)
+    subnet[subnet_data['name']+"_subnet"]['properties']['allocation_pools'][0]['start'] = str(address[2])
+    subnet[subnet_data['name']+"_subnet"]['properties']['allocation_pools'][0]['end'] = str(address[-2])
 
 
 data_all = {}
